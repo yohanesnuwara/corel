9663eab6ddcc192b46cb9574d7e58a9a509488d0
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2024-04-29 18:08:16 +0200

    add predict_facies function

--- corel.py
+++ corel.py
@@ -3,6 +3,7 @@ def setup():
   def pip_install(name):
     subprocess.call(['pip', 'install', name])
   pip_install('azure-storage-blob')
+  pip_install('ultralytics')  
 
 def download_model():
   from azure.storage.blob import BlobServiceClient
@@ -25,3 +26,19 @@ def download_model():
   with open("best (5).pt", "wb") as f:
       data = blob_client.download_blob()
       data.readinto(f)
+
+def predict_facies(path_to_core_image, path_to_model):
+  import matplotlib.pyplot as plt  
+  import ultralytics
+  ultralytics.checks()
+
+  model1 = YOLO(path_to_model)
+  result = model1.predict(source=path_to_core_image, 
+                 imgsz=640, iou=.3, save=True, 
+                 save_txt=True, save_conf=True, show_labels=False)  
+  
+  # Visualize result
+  for i in range(len(result)):
+      plt.figure(figsize=(5,5))
+      plt.imshow(result[i].plot())
+      plt.show()  
